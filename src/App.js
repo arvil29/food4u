4c0db19e7dc4bebe5ea775f2ad4fc03a8b5c60db
Arvil Dey <arvildey@gmail.com>
2020-09-05 15:35:57 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -2,85 +2,74 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 import Recipe from "./Recipe";
 import logo from "./logo.png";
-import MediaQuery from "react-responsive"
 
 
 const App = () => {
-const APP_ID = "58e90476";
-const APP_KEY = "2a9a84ead8ffd06632b15d340b660668";
+  const APP_ID = "58e90476";
+  const APP_KEY = "2a9a84ead8ffd06632b15d340b660668";
 
-//create state for getting recipes
-//every recipe call made from api stored inside recipes array
-//we're taking data from this state and passing it down to props
-const [recipes, setRecipes] = useState([]);
+  //create state for getting recipes
+  //every recipe call made from api stored inside recipes array
+  //we're taking data from this state and passing it down to props
+  const [recipes, setRecipes] = useState([]);
 
-//create state for search & set search to empty string
-const [search, setSearch] = useState("")
+  //create state for search & set search to empty string
+  const [search, setSearch] = useState("")
 
-//get query name to set into url to fetch
-const [query, setQuery] = useState("");
+  //get query name to set into url to fetch
+  const [query, setQuery] = useState("");
 
-//put query into useEffect so page is rendered only when search button clicked & query string is changed
-useEffect(() => {
-  getRecipes();
-}, [query]);
+  //put query into useEffect so page is rendered only when search button clicked & query string is changed
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
 
-const getRecipes = async () => {
-  const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
-  const data = await response.json();
-  //stores recipes from data.hits
-  setRecipes(data.hits);
-  console.log(data.hits);
-};
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    //stores recipes from data.hits
+    //hits is a component of json data retrieved --> check console in browser
+    setRecipes(data.hits);
+    console.log(data.hits);
+  };
 
-//function to update search box w/ typed data
-//we get event from this
-const updateSearch = e => {
-  setSearch(e.target.value);
-}
+  //function to update search box w/ typed data
+  //we get event from this
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  }
 
-//set query to whatever was typed inside searchbar
-const getSearch = e => {
-  e.preventDefault();
-  setQuery(search);
+  //set query to whatever was typed inside searchbar
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+  }
 
-}
-
-//'recipe' prop is defined below
-//every recipe is stored from recipes gotten above into recipe prop
-  return (
-    <div className="App">
-
-      
+  //'recipe' prop is defined below
+  //every recipe is stored from recipes gotten above into recipe prop
+  //2 components are returned 1st is form & 2nd is recipes
+    return (
+      <div className="App">
         <form onSubmit={getSearch} className="search-form">
-          
           <input placeHolder="Find recipe..." className="search-bar" type="text" value={search} onChange={updateSearch}/>
           <button className="search-button" type="submit">
             Search
           </button>
         </form>
-  
-      <MediaQuery query='(min-device-width: 768px)'>
-        <MediaQuery query='(max-device-width: 991px)'>
-          <form onSubmit={getSearch} className="search-form">
-            <input placeHolder="Find recipe..." className="search-bar" type="text" value={search} onChange={updateSearch}/>
-          </form>
-        </MediaQuery>
-      </MediaQuery>
-      
-      <div className="recipes">
-      {recipes.map(recipe =>(
-       <Recipe 
-          key={recipe.recipe.label}
-          title={recipe.recipe.label} 
-          calories={recipe.recipe.calories} 
-          image={recipe.recipe.image}
-          ingredients={recipe.recipe.ingredients}
-        />
-      ))}
+        
+        <div className="recipes">
+        {recipes.map(recipe =>(
+          <Recipe 
+              key={recipe.recipe.label}
+              title={recipe.recipe.label} 
+              calories={recipe.recipe.calories} 
+              image={recipe.recipe.image}
+              ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+        </div>
       </div>
-    </div>
-    );
+      );
   };
     
 
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import style from "./recipe.module.css"
 
+//displays every single individual recipe from api
+//info is passed in from App.js
+//accessed from App.js whenever <Recipe> obj is used
 const Recipe = ({title, calories, image, ingredients}) => {
     return(
         <div className={style.recipe}>
